eef6f6efd537796ee55bab94f73a7e458dbfc225
vasanthimunduri <160224315+vasanthimunduri@users.noreply.github.com>
2024-07-20 16:07:14 +0530

    Update sqlbase.sql

--- sqlbase.sql
+++ sqlbase.sql
@@ -4,3 +4,14 @@
 create table student1 (rolno number(20) primary key ,name varchar(20), age number(2),bname varchar(5),perc number(2,2),email varchar(30) unique);
 insert into student1 values(1,'vasanthi',18,'cse',98.2,'v@gmail.com');
 insert into student1 values(2,'vaishu',18,'cse',99.9,'va@gmail.com');
+
+
+
+
+
+
+
+
+create table students (rolno number(20) primary key ,name varchar(20), age number(2),bname varchar(5),perc number(2),email varchar(30) unique);
+insert into students values(1,'vasanthi',18,'cse',98,'v@gmail.com');
+insert into students  values(2,'vaishu',18,'cse',99,'va@gmail.com');
