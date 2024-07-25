bcfba9bff7302dbe96c3a5e4babea6484e2663f4
vasanthimunduri <160224315+vasanthimunduri@users.noreply.github.com>
2024-07-25 12:26:42 +0530

    Update sqlbase.sql

--- sqlbase.sql
+++ sqlbase.sql
@@ -56,3 +56,30 @@ truncate table students ;
 
 //deletingentiretablefromdatabase
 drop table students;
+
+
+
+
+
+
+
+
+
+
+
+
+//markstable
+
+create table marks (subcode varchar(50) primary key ,sub varchar(20),grade varchar(10),credits number(2));
+insert into marks values('23EE2T01','BEEE','A',3) ;
+insert into marks values('23BS2T05','DE&VC','A',3) ;
+insert into marks values('23BS2T02','EP','S',3) ;
+insert into marks values('23IT2T01','DS','A',3) ;
+insert into marks values('23IT2P02','IT WS','S',1) ;
+insert into marks values('23BS1P04','EP LAB','S',1) ;
+insert into marks values('23EE2P01','EEE WS','S',1) ;
+insert into marks values('23IT2P01','DS LAB','S',1) ;
+insert into marks values('23ME2T01','EG','B',3);
+insert into marks values('23BS2P05','SPORTS','S',1) ;
+SELECT *
+FROM marks ;
